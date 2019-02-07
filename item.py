7fa5e7fe1aae9639b157127f047838f8d60b5ca9
Brian Gomez <briangx92@gmail.com>
2019-02-07 15:46:51 -0500

    finished creating task and appending time

--- item.py
+++ item.py
@@ -21,32 +21,18 @@
 # # task = input("Create A New Task: ")
 # # file1.write("/n"+ task)
 # # file1.close()
+from datetime import datetime
 
-class Item():
-    def showAll():
-        readTask = open("todos.txt", "r")
-        readTask.readlines()
-        readTask.close()
 
-    def markComplete():
-        edit = open("todos.txt").read()
-        completedTask = input("What Task Did You Complete? ")
-        s = edit.replace(completedTask, completedTask + " This task is completed")
-        f2 = open("todos.txt", 'w+')
-        f2.write(s)
-        f2.close()
-
-        f = open('todos.txt','r')
-        message = f.read()
-        print(message)
-        f.close()
+class Item(object):
 
     def createTask():
+        now = datetime.now()
+        date_time = now.strftime("%Y/%m/%d %H:%M:%S")
         file1 = open("todos.txt","a+")
         task = input("Create A New Task: ")
-        file1.write("\n" + task)
-        file1.close()
-
+        file1.write("\n" + task + ' ' + date_time)
+        print(f"{task} task created! at {date_time}")
+        
 
 Item.createTask()
-Item.markComplete()
