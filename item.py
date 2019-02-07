0cf543d5f990413312b4c64c5ee6d1fe40153423
Brian Gomez <briangx92@gmail.com>
2019-02-07 15:50:01 -0500

    cleaned up some empty spaces

--- item.py
+++ item.py
@@ -1,29 +1,4 @@
-# # from sys import argv
-
-# # script, filename = argv
-
-# # txt = open(filename)
-
-# class Item(object):
-
-#     def create(self):
-#         file1 = open("todos.txt","a+")
-#         task = input("Create A New Task: ")
-#         file1.write("/n"+ task)
-#         file1.close() 
-# #         f = open('todos.txt','r')
-# #         message = f.read()
-# #         print(message)
-# #         f.close()
-# #         return self.create
-
-# # file1 = open("todos.txt","a+")
-# # task = input("Create A New Task: ")
-# # file1.write("/n"+ task)
-# # file1.close()
 from datetime import datetime
-
-
 class Item(object):
 
     def createTask():
@@ -33,6 +8,6 @@ class Item(object):
         task = input("Create A New Task: ")
         file1.write("\n" + task + ' ' + date_time)
         print(f"{task} task created! at {date_time}")
-        
+    
 
 Item.createTask()
