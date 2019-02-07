85de1a78b195e63c79546b6ff93c75cf0b25a54e
Brian Gomez <briangx92@gmail.com>
2019-02-07 16:28:18 -0500

    brushed up item class

--- item.py
+++ item.py
@@ -7,7 +7,30 @@ class Item(object):
         file1 = open("todos.txt","a+")
         task = input("Create A New Task: ")
         file1.write("\n" + task + ' ' + date_time)
-        print(f"{task} task created! at {date_time}")
+        print(f"{task} task created! at {date_time}.")
+        file1.close()
     
+    def isCompleted():
+        edit = open("todos.txt").read()
+        print(edit)
+        completedTask = input("What Task Did You Complete? ")
+
+        s = edit.replace(completedTask, "This task is completed")
+        f2 = open("todos.txt", 'w+')
+        if s != f2:
+            f2.write(s)
+        
+        elif s == f2:
+            f2.write(s)
+        else:
+            print("item not in list")
+        f2.close()
+
+        f = open('todos.txt','r')
+        message = f.read()
+        print(message)
+        f.close()
+
 
 Item.createTask()
+Item.isCompleted()
