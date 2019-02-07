3ae8a92b070771ac54838bed0852a15b0c240dc8
Brian Gomez <briangx92@gmail.com>
2019-02-07 13:15:14 -0500

    Got it to create a task and give the option of completing the task

--- item.py
+++ item.py
@@ -1,3 +1,52 @@
+# # from sys import argv
 
-class Item(object):
-    
+# # script, filename = argv
+
+# # txt = open(filename)
+
+# class Item(object):
+
+#     def create(self):
+#         file1 = open("todos.txt","a+")
+#         task = input("Create A New Task: ")
+#         file1.write("/n"+ task)
+#         file1.close() 
+# #         f = open('todos.txt','r')
+# #         message = f.read()
+# #         print(message)
+# #         f.close()
+# #         return self.create
+
+# # file1 = open("todos.txt","a+")
+# # task = input("Create A New Task: ")
+# # file1.write("/n"+ task)
+# # file1.close()
+
+class Item():
+    def showAll():
+        readTask = open("todos.txt", "r")
+        readTask.readlines()
+        readTask.close()
+
+    def markComplete():
+        edit = open("todos.txt").read()
+        completedTask = input("What Task Did You Complete? ")
+        s = edit.replace(completedTask, completedTask + " This task is completed")
+        f2 = open("todos.txt", 'w+')
+        f2.write(s)
+        f2.close()
+
+        f = open('todos.txt','r')
+        message = f.read()
+        print(message)
+        f.close()
+
+    def createTask():
+        file1 = open("todos.txt","a+")
+        task = input("Create A New Task: ")
+        file1.write("\n" + task)
+        file1.close()
+
+
+Item.createTask()
+Item.markComplete()
