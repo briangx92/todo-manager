8901d6adf4cfeb274b9f2e7617904ae700ff280c
Brian Gomez <briangx92@gmail.com>
2019-02-08 14:01:32 -0500

    restarted

--- manager.py
+++ manager.py
@@ -1,13 +1,5 @@
-from todos.py import function() #<-- arbitrary for now
+class Manager(object):
+    pass
 
-class Manager(object)
-
-    def __init__(self, todo, task):
-        self.todo = todo
-        self.task = task
-    
-    def taskmanager(self, print, item_name):
-        print = 
-        item_list = input("> ") 
 
 
