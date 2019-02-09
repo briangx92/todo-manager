b76fd7b1c4c21ad6caffd017911562f4ade1cb7a
Brian Gomez <briangx92@gmail.com>
2019-02-09 08:51:20 -0500

    Trying a different approach

--- item.py
+++ item.py
@@ -1,36 +1,61 @@
 from datetime import datetime
+from textwrap import dedent
 class Item(object):
 
+    # def createTask():
+    #     now = datetime.now()
+    #     date_time = now.strftime("%Y/%m/%d %H:%M:%S")
+    #     file1 = open("todos.txt","a+")
+    #     task = input("Create A New Task: ")
+    #     file1.write("\n" + task + ' ' + date_time)
+    #     print(f"{task} task created! at {date_time}.")
     def createTask():
         now = datetime.now()
         date_time = now.strftime("%Y/%m/%d %H:%M:%S")
-        file1 = open("todos.txt","a+")
-        task = input("Create A New Task: ")
-        file1.write("\n" + task + ' ' + date_time)
-        print(f"{task} task created! at {date_time}.")
-        file1.close()
-    
-    def isCompleted():
-        edit = open("todos.txt").read()
-        print(edit)
-        completedTask = input("What Task Did You Complete? ")
+        todofile = open("todos.txt", "a+")
+        print(dedent("""
+        Type help for list of commands
+        """))
+        new_task = input("What do you want to do?")
+        list = []
+        new_task = prompt
 
-        s = edit.replace(completedTask, "This task is completed")
-        f2 = open("todos.txt", 'w+')
-        if s != f2:
-            f2.write(s)
+        if new_task == "new todo":
+            
+
+
+
+
+
+
+
+
+
+
+
+
+    # def isCompleted():
+    #     edit = open("todos.txt").read()
+    #     print(edit)
+    #     completedTask = input("What Task Did You Complete? ")
+
+    #     s = edit.replace(completedTask, "This task is completed")
+    #     f2 = open("todos.txt", 'w+')
+    #     if s != f2:
+    #         f2.write(s)
         
-        elif s == f2:
-            f2.write(s)
-        else:
-            print("item not in list")
-        f2.close()
+    #     elif s == f2:
+    #         f2.write(s)
+    #     else:
+    #         print("item not in list")
+    #     f2.close()
 
-        f = open('todos.txt','r')
-        message = f.read()
-        print(message)
-        f.close()
+    #     f = open('todos.txt','r')
+    #     message = f.read()
+    #     print(message)
+    #     f.close()
 
 
 Item.createTask()
-Item.isCompleted()
+# Item.isCompleted()
+
--- manager.py
+++ manager.py
@@ -1,3 +1,4 @@
+
 class Manager(object):
     pass
 
