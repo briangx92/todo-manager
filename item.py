aa202c76b49925c0e881475f888bb2de65f56998
Brian Gomez <briangx92@gmail.com>
2019-02-10 10:35:04 -0500

    polished up manager

--- item.py
+++ item.py
@@ -1,11 +1,15 @@
 from datetime import datetime
 from textwrap import dedent
 from sys import exit
+
 class Item(object):
 
     def add_time():
         now = datetime.now()
         date_time = now.strftime("%Y/%m/%d %H:%M:%S")
+        todofile = open("todos.txt", "a+")
+
+        if 
 
 Item.add_time()
         
--- manager.py
+++ manager.py
@@ -2,39 +2,51 @@ from textwrap import dedent
 from sys import exit
 
 class Manager(object):
+    print(dedent("""
+    **********************************
+    * Type help for list of commands *
+    **********************************
+        """))
+    new_task = input("What do you want to do? > ")
 
-        if new_task == "new":
-            todofile = open(r"todos.txt", "a+")
-            print(dedent("""
-            **********************
-            * Create a new task  *
-            ********************** """))
-            new_todo = input("> ")
-            todofile.write(new_todo + " " + "\n")
-            todofile.close()
-            Manager.create_task()
+    if new_task == "new":
+        todofile = open(r"todos.txt", "a+")
+        print(dedent("""
+        *********************
+        * Create a new task *
+        ********************* """))
+        new_todo = input("> ")
+        todofile.write(new_todo + " " + "\n")
+        todofile.close()
+        prompt = input("Do you want to create a new task? yes or no?>  ")
 
-        elif new_task == 'list':
-            todofile = open("todos.txt", "r")
-            print(todofile.read())
-            todofile.close()
-            Manager.create_task
-
-        elif new_task == 'help':
-            print(dedent("""
-            *****************************
-            * new (creates a new task)  *
-            * list (list all the items) *
-            *****************************
-            """))
-            Manager.create_task()
+        if prompt == "yes":
+            Manager.new_task()
+            return 'new item created'
         else:
-            print("command not recognized")
-            prompt = input("Do you want to restart? yes or no?> ")
+            exit(0)
 
-            if prompt == 'yes':
-                Manager.create_task()
-            elif prompt == 'no':
-                exit(0)
+    elif new_task == 'list':
+        todofile = open("todos.txt", "r")
+        print(todofile.read())
+        todofile.close()
+        Manager.create_task
+
+    elif new_task == 'help':
+        print(dedent("""
+        *****************************
+        * new (creates a new task)  *
+        * list (list all the items) *
+        *****************************
+        """))
+        Manager.create_task()
+    else:
+        print("command not recognized")
+        prompt = input("Do you want to restart? yes or no?> ")
+
+        if prompt == 'yes':
+            Manager.create_task()
+        elif prompt == 'no':
+            exit(0)
         
 Manager.create_task()
