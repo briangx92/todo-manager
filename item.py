770156e1bfdfb3fc3b3b678cfd41b632cc4bd340
Brian Gomez <briangx92@gmail.com>
2019-02-10 08:01:53 -0500

    create functions in the manager.py and deleted some comments on item.py

--- item.py
+++ item.py
@@ -1,3 +1,46 @@
-
+from datetime import datetime
+from textwrap import dedent
 class Item(object):
-    
+
+    def createTask():
+        now = datetime.now()
+        date_time = now.strftime("%Y/%m/%d %H:%M:%S")
+        print(dedent("""
+        Type help for list of commands
+        """))
+        new_task = input("What do you want to do? > ")
+
+        if new_task == "new":
+            todofile = open(r"todos.txt", "a+")
+            print(dedent("""
+            Create a new task
+            """))
+            new_todo = input("> ")
+            todofile.write(new_todo + " "+ date_time + "\n")
+            todofile.close()
+            Item.createTask()
+
+        elif new_task == 'list':
+            todofile = open("todos.txt", "r")
+            print(todofile.read())
+            todofile.close()
+
+        elif new_task == 'help':
+            print(dedent("""
+            **************************
+            new (creates a new task)
+            list (list all the items)
+            **************************
+            """))
+            Item.createTask()
+        else:
+            print("command not recognized")
+            prompt = input("Do you want to restart? yes or no?> ")
+
+            if prompt == 'yes':
+                Item.createTask()
+            else:
+                pass
+
+Item.createTask()
+
--- manager.py
+++ manager.py
@@ -2,12 +2,15 @@ from todos.py import function() #<-- arbitrary for now
 
 class Manager(object)
 
-    def __init__(self, todo, task):
-        self.todo = todo
-        self.task = task
+    def __init__(self, todo_list, new_task, complete):
+        self.todo_list = todo_list
+        self.new_task = new_task
+        self.complete = complete
     
-    def taskmanager(self, print, item_name):
-        print = 
-        item_list = input("> ") 
+    def todo(self):
+        todo_file = open(r"todos.txt", "r")
+        item_list = input("> ")
 
+    def create(self):
+        todo
 
