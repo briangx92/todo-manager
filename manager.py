d16e2dfc6bd002e90f66db49b41adc3892181fdf
Brian Gomez <briangx92@gmail.com>
2019-02-10 16:00:13 -0500

    fixed an error

--- manager.py
+++ manager.py
@@ -3,16 +3,30 @@ from sys import exit
 
 class Manager(object):
 
+    def create_task():
+
+        print(dedent("""
+        **********************************
+        * Type help for list of commands *
+        **********************************
+            """))
+        new_task = input("What do you want to do? > ")
+
         if new_task == "new":
             todofile = open(r"todos.txt", "a+")
             print(dedent("""
-            **********************
-            * Create a new task  *
-            ********************** """))
+            *********************
+            * Create a new task *
+            ********************* """))
             new_todo = input("> ")
             todofile.write(new_todo + " " + "\n")
             todofile.close()
-            Manager.create_task()
+            prompt = input("Do you want to create a new task? yes or no?>  ")
+
+            if prompt == "yes":
+                Manager.create_task()
+            else:
+                exit(0)
 
         elif new_task == 'list':
             todofile = open("todos.txt", "r")
