139778c7f02b53a7b910ad45cea933d2da103913
Brian Gomez <briangx92@gmail.com>
2019-02-11 11:16:37 -0500

    imported all classes from item and manager.py

--- todos.py
+++ todos.py
@@ -1,6 +1,16 @@
-from sys import dedent
-
+from manager.py import Manager 
+from item.py import Item
+from textwrap import dedent
 class ToDoList(object):
+
+    def todosoftware():
+
+            Manager.create_task()
+            Item.add_time()
+            exit(0)
+
+ToDoList.todosoftware()
+
     
 
 
